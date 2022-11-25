9f6e9fe25bc4de1ba16e4450e4bd0caaac894bfe
eraDong <qq1184434988@gmail.com>
2022-11-25 17:16:30 +0800

    第六周周赛题目

---
+++ CodeContestWeekly(CCW)/Week6/Solutions/小卡与质数.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<cstring>
+#include<cmath>
+#include<algorithm>
+#include<stack>
+#include<unordered_map>
+#include<unordered_set>
+#include<set>
+#include<map>
+//#pragma GCC optimize(3)
+using namespace std;
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define scan(x)   scanf("%lld",&x)
+#define int long long
+#define lowbit(x) x&(-x) //二进制最低位所代表的数
+#define PI 3.1415926535
+inline char nc(){
+    static char buf[100000],*p1=buf,*p2=buf;
+    return p1==p2&&(p2=(p1=buf)+fread(buf,1,100000,stdin),p1==p2)?EOF:*p1++;
+}
+inline int _read(){
+    char ch=nc();int sum=0;
+    while(!(ch>='0'&&ch<='9'))ch=nc();
+    while(ch>='0'&&ch<='9')sum=sum*10+ch-48,ch=nc();
+    return sum;
+}
+typedef pair<int,int> PII;
+typedef long long ll;
+int gcd(int a,int b){
+	return b>0 ? gcd(b,a%b):a;
+}
+int exgcd(int a,int b,int &x,int &y)
+{
+	if(!b)
+	{
+		x = 1,y = 0;
+		return a;
+	}
+	int d = exgcd(b,a%b,y,x);
+	y-=a/b*x;
+	return d;
+}
+const int N = 2e6+10;
+const int mod = 1e9+7;
+const int INF = 0x3f3f3f3f;
+void init()
+{
+	cin.tie(0),cout.tie(0);
+	ios::sync_with_stdio(false);
+}
+
+int primes[N],cnt;
+bool st[N];
+
+void get_primes(int n)
+{
+	for(int i=2;i<=n;i++)
+	{
+		if(!st[i])primes[cnt++] = i;
+		for(int j=0;primes[j]<=n/i;j++)
+		{
+			st[primes[j]*i] = true;
+			if(i%primes[j]==0)break;
+		}
+	}
+}
+
+void solve()
+{
+	int a,b;
+	cin>>a>>b;
+	if((a==1&&b==2)||(a==2&&b==1))cout<<"Yes"<<endl;
+	else cout<<"No"<<endl;
+}
+
+signed main()
+{
+	init();
+	int t;
+	cin>>t;
+	while(t--)solve();
+}
---
+++ CodeContestWeekly(CCW)/Week6/Source.md
@@ -0,0 +1,18 @@
+<!--
+ * @Author: eraDong qq1184434988@gmail.com
+ * @Date: 2022-10-27 23:11:02
+ * @LastEditors: eraDong qq1184434988@gmail.com
+ * @LastEditTime: 2022-11-25 17:16:09
+ * @FilePath: \RandomThings\CodeContestWeekly(CCW)\Week2\Source.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<h1>选题<h1>
+
+Title1 [竞争得分](https://www.luogu.com.cn/problem/P8841)
+
+Title2 [小卡与质数](https://www.luogu.com.cn/problem/P8845)
+
+Title3 [小卡与质数2](https://www.luogu.com.cn/problem/P8842)
+
+<h1>候补题<h1>
+
